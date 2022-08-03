55ccd7ee6bbeb23c7dabc640342c6d75de9d8209
Joice Araújo <joicearaujoedu@gmail.com>
2022-08-03 10:53:58 -0300

    Criando pagina de login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import propType from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        Header
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: propType.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import propType from 'prop-types';
+import { connect } from 'react-redux';
+import { emailAdd } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  enable = () => {
+    const five = 5;
+    const { password, email } = this.state;
+    const example = /\S+@\S+\.\S+/;
+    if (example.test(email) && password.length > five) return false;
+    return true;
+  }
+
+  handleChance = (event) => {
+    const { email } = this.state;
+    const { emailId, history } = this.props;
+    event.preventDefault();
+    emailId(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <input
+          type="email"
+          placeholder="example@example.com"
+          data-testid="email-input"
+          onChange={ (event) => this.setState({
+            email: event.target.value,
+          }) }
+          value={ email }
+        />
+
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          data-testid="password-input"
+          onChange={ (event) => this.setState({
+            password: event.target.value,
+          }) }
+          value={ password }
+        />
+        <button
+          type="submit"
+          disabled={ this.enable() }
+          onClick={ (event) => this.handleChance(event) }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  emailId: (payload) => dispatch(emailAdd(payload)),
+});
+
+Login.propTypes = {
+  emailId: propType.func.isRequired,
+  history: propType.objectOf(propType.any).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
